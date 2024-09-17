2f82218aa0ae2af4623c01b8193740f7775c178c
cshafizadeh <shafizac@oregonstate.edu>
2024-09-17 14:20:08 -0700

    feat: updated UI
    - Updated NavBar typo
    - Updated NavBar button to open in new tab
    - Updated Title Card spacing on mobile
    - Updated Contact Card submit button hover background color
    - Created footer component

--- app/pages/hero.js
+++ app/pages/hero.js
@@ -1,7 +1,6 @@
 // import TitleCard from '../../components/ui/TitleCard';
 // import { TypewriterEffect } from '@/components/ui/typewriter-effect';
 
-
 // export default function HeroSection() {
 //   return (
 //             <div className="flex flex-row items-center justify-center gap-16 mt-8">
@@ -34,7 +33,7 @@ export default function HeroSection() {
   return (
     <div className="flex flex-col md:flex-row items-center justify-center md:gap-16 mt-8">
       {/* TitleCard */}
-      <div className="w-[80%] md:w-[70%] md:max-w-[400px] h-auto mb-4 md:mb-0">
+      <div className="w-[80%] md:w-[70%] md:max-w-[400px] h-auto mb-4 md:mb-0 pb-4">
         <TitleCard />
       </div>
 
@@ -42,18 +41,36 @@ export default function HeroSection() {
       <div className="w-[80%] md:w-[80%] md:max-w-[400px] h-auto p-4">
         <TypewriterEffect
           words={[
-            { text: "What", className: "font-satoshiBold text-primaryOrange text-[28px] md:text-[36px]" },
-            { text: "Do", className: "font-satoshiBold text-primaryOrange text-[28px] md:text-[36px]" },
-            { text: "We", className: "font-satoshiBold text-primaryOrange text-[28px] md:text-[36px]" },
-            { text: "Do ?", className: "font-satoshiBold text-primaryOrange text-[28px] md:text-[36px]" }
+            {
+              text: 'What',
+              className:
+                'font-satoshiBold text-primaryOrange text-[28px] md:text-[36px]',
+            },
+            {
+              text: 'Do',
+              className:
+                'font-satoshiBold text-primaryOrange text-[28px] md:text-[36px]',
+            },
+            {
+              text: 'We',
+              className:
+                'font-satoshiBold text-primaryOrange text-[28px] md:text-[36px]',
+            },
+            {
+              text: 'Do ?',
+              className:
+                'font-satoshiBold text-primaryOrange text-[28px] md:text-[36px]',
+            },
           ]}
         />
         <p className="font-satoshiMedium text-[14px] md:text-[16px] max-w-full md:max-w-3xl text-left text-strongBlack mt-4">
-          At the Oregon Software Consulting Group, we provide real-world experience in software
-          consulting by working on hands-on projects and case studies. Our goal is to help students
-          develop practical skills in problem-solving, project management, and client communication.
-          We bring in industry experts to share insights and offer mentorship, giving our members the
-          tools and knowledge needed to succeed in the fast-paced world of software consulting.
+          At the Oregon Software Consulting Group, we provide real-world
+          experience in software consulting by working on hands-on projects and
+          case studies. Our goal is to help students develop practical skills in
+          problem-solving, project management, and client communication. We
+          bring in industry experts to share insights and offer mentorship,
+          giving our members the tools and knowledge needed to succeed in the
+          fast-paced world of software consulting.
         </p>
       </div>
     </div>
